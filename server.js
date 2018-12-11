06747300e523cc0e7648fb81cf94dbec4d284dfa
LandrumTrev <landrum.trevillian@gmail.com>
2018-12-11 17:07:10 -0500

    start scrape route data targeting

--- server.js
+++ server.js
@@ -25,14 +25,37 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/spacescraperdb", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/spacescraperdb";
+
+mongoose.connect(MONGODB_URI);
 
 // ROUTES
 
+// GET route to scrape News from spacenews.com
+// https://spacenews.com/segment/news/
+app.get("/scrape", function(req, res) {
+  axios.get("https://spacenews.com/segment/news/").then(function(response) {
+    // console.log(response);
+    // console.log(response.data);
+    var $ = cheerio.load(response.data);
+    // console.log($);
 
+    $(".launch-article").each(function(i, element) {
+      // console.log(element);
+
+      // "result" is an object to store article data
+      var result = {};
+
+      var title = $('.launch-title').children("a").text();
+      // var title = $(this).children("a").text();
+      console.log(title + "\n");
+      // console.log($(this).children("h2").text());
+
+    }); // end cheerio.each
+  }); // end axios.get.then
+}); // end app.get
 
 // SERVER
 app.listen(PORT, function() {
   console.log("Listening for space being scraped on PORT " + PORT + "!");
 });
-
